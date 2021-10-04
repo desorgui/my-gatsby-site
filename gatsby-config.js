41270c18e6d0ee6bde5494c4c2d583f07b733240
desorgui <desorguishny@gmail.com>
2021-10-04 15:08:53 -0400

    adding blog list

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
 		siteUrl: "https://www.yourdomain.tld",
 		title: "My Gatsby Site",
 	},
-	plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "blog",
+				path: `${__dirname}/blog`,
+			},
+		},
+	],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,7 @@ const HomePage = () => {
 			<p>
 				Hi there! I'm the proud creator of this site, which I built with Gatsby.
 			</p>
-			<StaticImage
-				alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-				src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-			/>
+
 			<StaticImage
 				alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
 				src="../images/design.jpg"
